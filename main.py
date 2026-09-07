f907c928e4e2c4c57a15bff2876a63d4351dc0ea
Peter Christian Briones <peterchristian.briones@wvsu.edu.ph>
2026-09-07 16:56:00 +0800

    doctstrings

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 def greet(name):
+    """First function"""
     print(f"Hello, {name}!")
 
 
 greet("World")
 
 def addition(a, b):
+    """Second function"""
     return a + b
 
 print(addition(5, 3))
